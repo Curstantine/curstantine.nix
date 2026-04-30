3afb94e99c36c4019a9663a394678bf3302ba1c0
Curstantine <Curstantine@proton.me>
2026-04-30 12:30:57 +0530

    feat: acu specific mangohud

--- modules/games.nix
+++ modules/games.nix
@@ -9,7 +9,28 @@
     enable = true;
     settingsPerApplication = {
       "wine-ACU" = {
-        full = true;
+        position = "bottom-left";
+        table_columns = 4;
+
+        font_size = 13;
+        no_small_font = true;
+        background_alpha = 0.4;
+        round_corners = 0;
+
+        cpu_stats = true;
+        cpu_temp = true;
+        cpu_power = true;
+        ram = true;
+
+        gpu_stats = true;
+        gpu_temp = true;
+        gpu_power = true;
+        vram = true;
+        pci_dev = "0000:01:00.0";
+
+        frame_timing = false;
+
+        fps_sampling_period = 500;
       };
     };
   };
