5e0cc52bf74d481f5a7a77f8b3c914500f000226
Curstantine <Curstantine@proton.me>
2026-07-23 19:11:44 +0530

    fix: broken node devshell

--- templates/devshell/node.nix
+++ templates/devshell/node.nix
@@ -3,8 +3,10 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
+
   outputs =
     {
+      self,
       nixpkgs,
       flake-utils,
     }:
@@ -12,29 +14,29 @@
       system:
       let
         pkgs = import nixpkgs { inherit system; };
-        nodeVersionStr = builtins.readFile ./.node-version;
-        nodeMajorVersion = builtins.head (builtins.match "v?([0-9]+).*" nodeVersionStr);
+
+        nodeVersionRaw = pkgs.lib.strings.trim (builtins.readFile ./.node-version);
+        nodeMajorVersion = builtins.head (builtins.match "v?([0-9]+).*" nodeVersionRaw);
         node = pkgs."nodejs_${nodeMajorVersion}";
-        # Dependencies required at run-time.
+
         buildInputs = with pkgs; [
           node
           corepack
-          postgresql
         ];
-        # Dependencies required at build-time.
+
         nativeBuildInputs = with pkgs; [ ];
       in
       {
-        # Development shell with: nix develop
         devShells.default = pkgs.mkShell {
-          buildInputs =
+          nativeBuildInputs =
             with pkgs;
             [
               nil
               ripgrep
             ]
-            ++ buildInputs
             ++ nativeBuildInputs;
+
+          buildInputs = buildInputs;
         };
       }
     );
